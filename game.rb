b7c27a4e9ed841e24493f4499e87a5715a61030e
Chris De Asis <138704176+cde-as@users.noreply.github.com>
2024-05-26 16:14:32 -0700

    app runs but with a bug relating to the answer

--- game.rb
+++ game.rb
@@ -9,19 +9,47 @@ class Game
   end
 
   def play
+    until game_over?
+      question = Question.new
+      question.ask_math_question(@current_player.name)
+      print "> "
+      answer = gets.chomp
+
+      if question.correct?(answer)
+        puts "YES! You are correct."
+      else
+        puts "Seriously? No!"
+        @current_player.lose_a_life
+      end
+
+      show_scores
+      switch_turns
+      puts "----- NEW TURN -----" unless game_over?
+    end
+
+    winner
+    puts "----- GAME OVER -----"
   end
+
 
   def switch_turns
+    @current_player = (@current_player == @player1) ? @player2 : @player1
   end
 
-  def game_over 
+  def game_over?
+    !@player1.alive? || !@player2.alive?
   end
 
-  def show_score
+  def show_scores
+    puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
   end
 
   def winner
-  end
+    if @player1.alive?
+      puts "Player 1 wins with a score of #{@player1.lives}/3"
+    else
+      puts "Player 2 wins with a score of #{@player2.lives}/3"
+    end
+end
 
 end
-    
--- player.rb
+++ player.rb
@@ -10,4 +10,7 @@ class Player
     @lives -= 1
   end
 
+  def alive? 
+    @lives > 0
+  end
 end
